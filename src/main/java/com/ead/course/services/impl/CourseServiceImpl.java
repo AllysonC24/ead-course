f65c1ffab2f2123cdfd9cb987ce596749b9fc9b6
Allyson Costa <allyson.wrk2024@gmail.com>
2024-12-06 01:32:23 -0300

    implementado método de deleção customizado CASCADE ALL

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,13 +1,44 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CourseServiceImpl implements CourseService {
 
     final CourseRepository courseRepository;
+    final LessonRepository lessonRepository;
+    final ModuleRepository moduleRepository;
+
+
+    //Metodo de deleção CASCADE ALL customizado
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+
+        if(!moduleModelList.isEmpty()){
+
+            for(ModuleModel moduleModel : moduleModelList){
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+                if(!lessonModelList.isEmpty()){
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            }
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courseRepository.delete(courseModel);
+    }
 }
--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,13 +1,32 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ModuleServiceImpl implements ModuleService {
 
     final ModuleRepository moduleRepository;
+    final LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel moduleModel) {
+
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+
+        if(!lessonModelList.isEmpty()){
+            lessonRepository.deleteAll(lessonModelList);
+        }
+        moduleRepository.delete(moduleModel);
+    }
 }
