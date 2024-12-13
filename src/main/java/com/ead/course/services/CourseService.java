ec875407384c419ba63212b512998c8f4e5ea8d8
Allyson Costa <allyson.wrk2024@gmail.com>
2024-12-13 06:38:00 -0300

    implementado métodos HTTP GET, POST, PUT, DELETE no controller de courses

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,24 @@
 package com.ead.course.services;
 
+import com.ead.course.dtos.CourseRecordDTO;
 import com.ead.course.models.CourseModel;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CourseService {
 
     void delete(CourseModel courseModel);
+
+    CourseModel save(CourseRecordDTO courseRecordDTO);
+
+    boolean existsByName(String name);
+
+    List<CourseModel> findAll();
+
+    Optional<CourseModel> findById(UUID courseId);
+
+    CourseModel update(CourseModel courseModel, CourseRecordDTO courseRecordDTO);
 }
--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.dtos.CourseRecordDTO;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
@@ -8,10 +9,15 @@ import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -27,18 +33,61 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public void delete(CourseModel courseModel) {
 
-        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        List<ModuleModel> moduleModelList = this.moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
 
         if(!moduleModelList.isEmpty()){
 
             for(ModuleModel moduleModel : moduleModelList){
-                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+                List<LessonModel> lessonModelList = this.lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
                 if(!lessonModelList.isEmpty()){
-                    lessonRepository.deleteAll(lessonModelList);
+                    this.lessonRepository.deleteAll(lessonModelList);
                 }
             }
-            moduleRepository.deleteAll(moduleModelList);
+            this.moduleRepository.deleteAll(moduleModelList);
         }
-        courseRepository.delete(courseModel);
+        this.courseRepository.delete(courseModel);
     }
+
+    @Override
+    public CourseModel save(CourseRecordDTO courseRecordDTO) {
+
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseRecordDTO, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return this.courseRepository.save(courseModel);
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return this.courseRepository.existsByName(name);
+    }
+
+    @Override
+    public List<CourseModel> findAll() {
+        return this.courseRepository.findAll();
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+
+        var courseModelOptional = this.courseRepository.findById(courseId);
+
+        if(courseModelOptional.isEmpty()){
+            //
+        }
+
+        return courseModelOptional;
+    }
+
+    @Override
+    public CourseModel update(CourseModel courseModel, CourseRecordDTO courseRecordDTO) {
+
+        BeanUtils.copyProperties(courseRecordDTO, courseModel);
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return this.courseRepository.save(courseModel);
+    }
+
 }
